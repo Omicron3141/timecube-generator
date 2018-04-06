1510c9247ccda08023faa620aa8e741244f1e4fd
Loaner <Loaner@WL-219-125.WPA.Claremont.Edu>
2018-04-05 22:33:44 -0700

    timed reloading added

--- static/js/Main.js
+++ static/js/Main.js
@@ -1,3 +1,10 @@
+var timer = null;
+
+$(document).ready(function() {
+  $("#timeSlider").on("input", onSliderChange);
+
+});
+
 function loadNew(){
   $.get("/new", function(data, status){
     if (status == "success") {
@@ -9,4 +16,20 @@ function loadNew(){
 
 function populateNewSentence(data){
 	$("#quote").html(data);
+}
+
+function onSliderChange(){
+	var text = "";
+	var val = $("#timeSlider").val();
+	if (timer != null){
+		clearInterval(timer);
+	}
+	if (val == 0){
+		text = "Manual";
+	} else {
+		text = "Auto: every "+val+"s";
+		timer = setInterval(loadNew, val*1000);
+	}
+	$("#sliderlabel").html(text);
+
 }
